d555d667581458a86a7b752b6e7c4bc17f74bb66
Chad M Chambers <61452826+CMChambers@users.noreply.github.com>
2025-10-17 14:15:37 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace CIS_207_Project_5_Student_Report_Card
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
