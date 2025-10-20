dd378ba82285c2444b2c5ca2cc5861b400ada816
Chad M Chambers <61452826+CMChambers@users.noreply.github.com>
2025-10-20 11:38:18 -0400

    add/ abstract base class for validationResult

--- ValidationResultString.cs
+++ ValidationResultString.cs
@@ -2,21 +2,15 @@
 
 namespace CIS_207_Project_5_Student_Report_Card
 {
-    internal class ValidationResultString
+    internal class ValidationResultString : ValidationResultBase
     {
-        private bool isError;
-        private string errorMessage = string.Empty;
-        private string errorTitle = string.Empty;
         private string value;
 
-        public bool IsError                                                     // flag to show successful validation
-        { get => isError; set => isError = value; }
-        public string ErrorMessage                                              // the error message for an unsuccessful validation
-        { get => errorMessage; set => errorMessage = value; }
-        public string ErrorTitle                                                // the error message box title for unsuccessful validation
-        { get => errorTitle; set => errorTitle = value; }
         public string Value                                                     // the validated value
-        { get => value; set => this.value = value; }
+        {
+            get => value;
+            set => this.value = value;
+        }
 
         public ValidationResultString(string _errorMessage, string _errorTitle) // constructor for an error result
         {
